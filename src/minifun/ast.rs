2c02e48d1b7f1cfde9c5d49a747ee7e987cceed8
NourHamid04 <nourhamid312@gmail.com>
2026-03-26 15:10:17 +0100

    minifun AST

--- src/minifun/ast.rs
+++ src/minifun/ast.rs
@@ -1,1 +1,21 @@
-// MiniFun abstract syntax tree will go here later.
+#[derive(Debug, Clone)]
+pub enum Term {
+    Int(i64),
+    True,
+    False,
+    Var(String),
+
+    Fun(String, Box<Term>),
+    App(Box<Term>, Box<Term>),
+
+    Add(Box<Term>, Box<Term>),
+    Sub(Box<Term>, Box<Term>),
+    Mul(Box<Term>, Box<Term>),
+    And(Box<Term>, Box<Term>),
+    Less(Box<Term>, Box<Term>),
+    Not(Box<Term>),
+
+    If(Box<Term>, Box<Term>, Box<Term>),
+    Let(String, Box<Term>, Box<Term>),
+    LetFun(String, String, Box<Term>, Box<Term>),
+}
