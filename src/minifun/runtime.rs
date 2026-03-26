d0e414cd51e7605824432361eeb4eb51dd02b43d
NourHamid04 <nourhamid312@gmail.com>
2026-03-26 15:15:51 +0100

    minifun Env

--- src/minifun/runtime.rs
+++ src/minifun/runtime.rs
@@ -1,1 +1,20 @@
-// MiniFun runtime environment will go here later.
+use std::collections::HashMap;
+
+use crate::minifun::ast::Term;
+
+// Values produced by MiniFun evaluation.
+#[derive(Debug, Clone)]
+pub enum Value {
+    Int(i64),
+    Bool(bool),
+
+    // Non-recursive closure: parameter, body, saved environment
+    Closure(String, Box<Term>, Env),
+
+    // Recursive closure: function name, parameter, body, saved environment
+    RecClosure(String, String, Box<Term>, Env),
+}
+
+/// MiniFun environment:
+/// maps variable names to values.
+pub type Env = HashMap<String, Value>;
